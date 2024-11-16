621edc689f319f6a2d978e398a5d921d9083363c
Shashwath-Karkera <shashwathkarkera119@gmail.com>
2024-11-16 19:54:18 +0530

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,82 @@
+export const cakes = [
+    {
+      id: 1,
+      name: "Chocolate Cake",
+      category: "Chocolate Cakes",
+      description: "Rich and creamy chocolate cake with layers of ganache.",
+      price: 25,
+      image:
+        "https://m.media-amazon.com/images/I/51IXbcL2pjL._AC_UF1000,1000_QL80_.jpg",
+      ingredients: "Chocolate, Flour, Eggs, Sugar",
+      sizes: ["Small", "Medium", "Large"],
+    },
+    {
+      id: 2,
+      name: "Dark Chocolate Cake",
+      category: "Chocolate Cakes",
+      description: "Dark chocolate cake with a rich chocolate mousse filling.",
+      price: 35,
+      image:
+        "https://www.cakeie.com/public/images/2%20Tier%20Dark%20Chocolate%20Cake-img1-cakeie.webp",
+      ingredients: "Dark Chocolate, Flour, Butter, Eggs, Sugar",
+      sizes: ["Medium", "Large"],
+    },
+  
+    {
+      id: 3,
+      name: "Fruit Cake",
+      category: "Fruit Cakes",
+      description: "Fresh fruit cake with seasonal fruits.",
+      price: 56,
+      image:
+        "https://www.cakesquarechennaionline.com/wp-content/uploads/2024/08/Fresh-Fruits-1-kg-Special-Birthday-Cake-by-Cake-Square-Chennai-Best-cake-shops-in-Chennai-Cakes-near-me.jpg",
+      ingredients: "Fruits, Flour, Sugar, Butter",
+      sizes: ["Small", "Medium"],
+    },
+    {
+      id: 4,
+      name: "Berry Fruit Cake",
+      category: "Fruit Cakes",
+      description: "Berry-filled fruit cake with a creamy frosting.",
+      price: 78,
+      image: "https://cdn7.kiwilimon.com/brightcove/8188/640x640/8188.jpg.webp",
+      ingredients: "Berries, Flour, Sugar, Butter",
+      sizes: ["Medium", "Large"],
+    },
+  
+    {
+      id: 5,
+      name: "Vanilla Cake",
+      category: "Vanilla Cakes",
+      description: "Soft and fluffy vanilla cake with cream frosting.",
+      price: 67,
+      image:
+        "https://m.media-amazon.com/images/I/71hLCH16ylL._AC_UF1000,1000_QL80_.jpg",
+      ingredients: "Vanilla, Flour, Eggs, Sugar, Cream",
+      sizes: ["Small", "Medium"],
+    },
+    {
+      id: 6,
+      name: "Classic Vanilla Cake",
+      category: "Vanilla Cakes",
+      description: "Classic vanilla cake with a simple buttercream frosting.",
+      price: 87,
+      image:
+        "https://bakebuddy.in/cdn/shop/files/Classic_Vanilla_Cake_c74fc02e-2f6b-404e-acba-048979949fed.jpg?v=1706443868",
+      ingredients: "Vanilla, Butter, Sugar, Eggs",
+      sizes: ["Medium", "Large"],
+    },
+  
+    {
+      id: 7,
+      name: "Custom Birthday Cake",
+      category: "Custom Cakes",
+      description: "Customize your cake for any occasion!",
+      price: 45,
+      image:
+        "https://bakeoffcakes.in/wp-content/uploads/2023/07/7xm.xyz526913.jpg",
+      ingredients: "Choose from Chocolate, Vanilla, or Fruit base.",
+      sizes: ["Small", "Medium", "Large"],
+    },
+  ];
+  
